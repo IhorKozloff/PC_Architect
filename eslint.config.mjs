e25ae7dd8906440f6b552631ccaeb75d4c9ca6e6
IhorKozloff <ik.eyeshield21@gmail.com>
2026-09-22 12:17:07 +0300

    github actions testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,35 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      quotes: ['warn', 'single'],
+      'jsx-quotes': ['warn', 'prefer-double'],
+      indent: ['warn', 2],
+      semi: ['warn', 'always'],
+      'key-spacing': ['warn', { beforeColon: false }],
+      'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 0 }],
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
-import { TypographyH1 } from "@/components/ui/typography-h1";
-import { TypographyH3 } from "@/components/ui/typography-h3";
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { TypographyH1 } from '@/components/ui/typography-h1';
+import { TypographyH3 } from '@/components/ui/typography-h3';
 
 export default function Home() {
+  const x = 5;
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-       <TypographyH1>Hello World!</TypographyH1>
-       <TypographyH3>Hello World!</TypographyH3>
-       <Button variant="outline">Button</Button>
+        <TypographyH1>Hello World!</TypographyH1>
+        <TypographyH3>Hello World!</TypographyH3>
+        <Button variant="outline">Button</Button>
       </main>
     </div>
   );
